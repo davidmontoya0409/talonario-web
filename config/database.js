e1798bc76321c06552d4b06f74b0aca47a3f8a25
davidmontoya0409 <david1452367@gmail.com>
2024-09-04 18:10:23 +0200

    subiendo proyecto de produccion

---
+++ config/database.js
@@ -0,0 +1,1 @@
+const{Sequelize:e}=require("sequelize");require("dotenv").config();const sequelize=new e({host:process.env.DB_HOST,port:process.env.DB_PORT,username:process.env.DB_USER,password:process.env.DB_PASSWORD,database:"loteria_db",dialect:"mysql",logging:!1});module.exports=sequelize;
---
+++ models/compra.js
@@ -0,0 +1,1 @@
+const{Model:e,DataTypes:l}=require("sequelize"),sequelize=require("../config/database");class Compra extends e{}Compra.init({nombreComprador:{type:l.STRING,allowNull:!1},numeroPuesto:{type:l.INTEGER,allowNull:!1,unique:!0},nombreResponsable:{type:l.STRING,allowNull:!1}},{sequelize,modelName:"Compra",tableName:"compras"}),module.exports=Compra;
